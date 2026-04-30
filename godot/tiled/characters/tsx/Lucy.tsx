087e6bdae2b4d89ab9d3053b4c5a63ba8854dd20
C8102E-A2A569 <operatingsystem.c8102e@gmail.com>
2026-04-30 19:18:54 +0300

    first commit

---
+++ godot/tiled/characters/tsx/Lucy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Lucy" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="../Lucy_idle_32x32.png" width="128" height="64"/>
+</tileset>
---
+++ godot/tiled/interior/tsx/interior.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="interior" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../interior.png" width="512" height="512"/>
+ <tile id="177">
+  <objectgroup draworder="index" id="3">
+   <object id="19" x="0.03125" y="0.03125">
+    <polygon points="0,0 31.9688,-0.03125 32,11.9375 -0.03125,11.9063"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
